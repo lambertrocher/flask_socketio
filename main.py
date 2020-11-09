3d4e218c5ec0d88d9fd59d1067c32d55da859fdd
lambertrocher <lambert.rocher@gmail.com>
2020-11-09 20:30:14 +0100

    (wip) add authentication

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-from flask import Flask
-from flask import render_template
-from flask_socketio import SocketIO
-from flask_socketio import send
-from flask_socketio import emit
 import socketio
+from flask import Flask, render_template, request
+from flask_login import LoginManager, login_user
+from flask_socketio import SocketIO, send, emit
+
+from user import User
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.secret_key = 'my secret key'
+login_manager = LoginManager()
+login_manager.init_app(app)
 socketio = SocketIO(app=app)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
 x = 0
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User(request.form['pseudo']) 
+        login_user(user)
+        #TODO validate flask.request.args.get('next') to prevent vulnerability to open redirect
+    return render_template('index.j2')
 
 @app.route('/home')
 def home():
@@ -23,6 +37,10 @@ def handle_message(message):
     print('received event: ', message)
     x += 1
     emit('my event', x)
+
+@socketio.on('connect')
+def connect():
+    emit('my response', {'data': 'Connected'})
 
 if __name__ == "__main__":
     socketio.run(app)
---
+++ user.py
@@ -0,0 +1,27 @@
+from flask_login import UserMixin
+
+users = {}
+
+class User:
+    def __init__(self, id: str) -> None:
+        self.id = id
+        self.authenticated = False
+        self.active = True
+        self.anonymous = False
+        users[id] = self
+
+    def get_id(self):
+        return self.id
+
+    def is_authenticated(self):
+        return self.authenticated
+
+    def is_active(self):
+        return self.active
+
+    def is_anonymous(self):
+        return self.anonymous
+
+    @staticmethod
+    def get(user_id):
+        return users.get(user_id)
